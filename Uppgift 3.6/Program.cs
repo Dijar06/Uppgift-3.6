509fa3080b9ca411bf0537e8db7e6c8d955fef35
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-06 12:58:42 +0200

    Add project files.

---
+++ Uppgift 3.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt förnamn först och därefter ditt efternamn");
+            string förnamn = Console.ReadLine();
+            string efternamn = Console.ReadLine();
+
+            if (förnamn.CompareTo(efternamn) < 0)
+            {
+                Console.WriteLine(förnamn + " kommer före " + efternamn + " i bokstavsordning");
+            }
+            else
+            {
+                Console.WriteLine(efternamn + " kommer före " + förnamn + " i bokstavsordning");
+            }
+        }
+    }
+}
